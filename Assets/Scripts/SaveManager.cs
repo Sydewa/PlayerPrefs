94f889db27d2ab3768b6a35fbe21a135cb228ecb
Sydewa <95858551+Sydewa@users.noreply.github.com>
2022-12-16 12:16:41 +0100

    16_12_2022

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -20,22 +20,30 @@ public class SaveManager : MonoBehaviour
 
     void LoadData()
     {
-        nameText.text = "Your name is: " + PlayerPrefs.GetString("name", "Your mom!");
+        nameText.text = "Your name is: " + PlayerPrefs.GetString("name", "Name");
         scoreText.text = "Your score is " + PlayerPrefs.GetInt("score", 0).ToString() + "!";
         positionText.text = "Your position is: " + PlayerPrefs.GetFloat("posX", 0).ToString() + "x, " + PlayerPrefs.GetFloat("posY", 0).ToString() + "y, " + PlayerPrefs.GetFloat("posZ", 0).ToString() + "z.";
     }
 
-    void SaveData()
+    public void SaveData()
     {
         PlayerPrefs.SetString("name", userName);
         PlayerPrefs.SetInt("score", userScore);
         PlayerPrefs.SetFloat("posX", userPosition.x);
         PlayerPrefs.SetFloat("posY", userPosition.y);
         PlayerPrefs.SetFloat("posZ", userPosition.z);
+
+        LoadData();
     }
 
-    void DelteData()
+    public void DeleteData()
     {
+        PlayerPrefs.DeleteKey("name");
+        PlayerPrefs.DeleteKey("score");
+        PlayerPrefs.DeleteKey("posX");
+        PlayerPrefs.DeleteKey("posY");
+        PlayerPrefs.DeleteKey("posZ");
 
+        LoadData();
     }
 }
